9a567722a0e9b14f27a9729c7a54b92bfc285ffd
abequar <ramiresbaptista@gmail.com>
2020-03-01 18:02:41 -0300

    Insere novos ícones e corrige js do comoChama.js

--- assets/comochama.js
+++ assets/comochama.js
@@ -1,9 +1,14 @@
 // código elaborado por https://github.com/GuiiMoraes confira o original em https://codepen.io/guiimoraes/pen/JjPRmvm
 function comoChama() {
   event.preventDefault();
-  console.log("phe");
-  const name = document.querySelector(".pergunta").value;
-  console.log(name);  
-	document.querySelector(".resposta").textContent = `${name}, tem algumas coisas minhas abaixo, fica à vontade`;
+  name = document.querySelector(".pergunta").value;
+  if (name == false) { // false entende que uma string vazia é falso poderia também ser '' no lugar de false
+    name = "Cibernauta";
+    document.querySelector(".hidden").style.display = "block";
+  } else {
+    document.querySelector(".resposta").textContent = `${name}`
+    document.querySelector(".hidden").style.display = "block";
+    document.querySelector(".hidden").style.transition = "all 2s";
+  }
 }
 // gostou? alguma sugestão? fala comigo!
